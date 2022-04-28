d322d1c83dc858988812bfa77ff0d3e8ac6ac220
Timur Khairutdinov <tmkhairutdinov@gmail.com>
2022-04-28 21:04:32 +0300

    Add project files.

---
+++ PathKnower.FAF.Replay.Parser.Common/Models/ReplayContent/RawReplayHeader.cs
@@ -0,0 +1,22 @@
+﻿namespace FAF.ReplayParser.Core.Models.ReplayContent
+{
+    public class RawReplayHeader
+    {
+        public long uid { get; set; }
+        public bool complete { get; set; }
+        public string state { get; set; }
+        public string featured_mod { get; set; }
+        public string game_type { get; set; }
+        public string recorder { get; set; }
+        public string host { get; set; }
+        public double launched_at { get; set; }
+        public double game_end { get; set; }
+        public string title { get; set; }
+        public string mapname { get; set; }
+        public int num_players { get; set; }
+        public int version { get; set; }
+        public string compression { get; set; }
+        public IEnumerable<Dictionary<string, string[]>> teams { get; set; }
+        public Dictionary<string, int> featured_mod_versions { get; set; }
+    }
+}
